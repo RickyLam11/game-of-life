91220c35f5b5aac833214cd7cd88e919b75e02dc
Ricky From Hong Kong <lamricky11@hotmail.com>
2022-08-26 01:48:33 +0200

    feat: add Node and init node from Grid

--- src/grid/grid.ts
+++ src/grid/grid.ts
@@ -1,21 +1,49 @@
 import { CanvasLayer } from '@/canvas-layer'
 import { Settings } from '@/settings'
 import { Entity, Vector2D } from '@/utils'
+import { Node } from '@/node'
 
 export class Grid extends Entity {
+  private _board: boolean[][] = []
+  private _nodes: Node[] = []
+
+  public get Nodes(): Node[] {
+    return this._nodes
+  }
+
   public Awake(): void {
+    // Awake components
+    super.Awake()
+
+    this.DrawBackground()
+
+    this.DrawGrid()
+    this.InitBoardAndNode()
+
+    // Awake child Entity (Node)
+    for (const node of this.Nodes) {
+      node.Awake()
+    }
+  }
+
+  public Update(deltaTime: number): void {
+    // Update components
+    super.Update(deltaTime)
+
+    this.UpdateNodeActive()
+
+    // Update child Entity (Node)
+    for (const node of this.Nodes) {
+      node.Update(deltaTime)
+    }
+  }
+
+  private DrawBackground(): void {
     CanvasLayer.Background.FillRect(
       new Vector2D(0, 0),
       new Vector2D(CanvasLayer.Background.Element.width, CanvasLayer.Background.Element.height),
       Settings.grid.background.color
     )
-
-    this.drawGrid()
-    this.DrawGrid()
-  }
-
-  public Update(deltaTime: number): void {
-    /* */
   }
 
   private DrawGrid(): void {
@@ -40,6 +68,35 @@ export class Grid extends Entity {
         Settings.grid.stroke.width,
         Settings.grid.stroke.color
       )
+    }
+  }
+
+  private InitBoardAndNode(): void {
+    const rowCount = Math.ceil(window.innerHeight / Settings.grid.node.size)
+    const columnCount = Math.ceil(window.innerWidth / Settings.grid.node.size)
+    const size = Settings.grid.node.size
+
+    for (let i = 0; i < rowCount; i++) {
+      const row = []
+      for (let j = 0; j < columnCount; j++) {
+        // node
+        const start = new Vector2D(j * size, i * size)
+        const end = new Vector2D(start.x + size, start.y + size)
+        const index = new Vector2D(i, j)
+
+        const node = new Node(start, end, index)
+        this.Nodes.push(node)
+        node.IsActive = (Math.random() < 0.1)
+        // board
+        row.push(node.IsActive)
+      }
+      this._board.push(row)
+    }
+  }
+
+  private UpdateNodeActive(): void {
+    for (const node of this.Nodes) {
+      node.IsActive = (Math.random() < 0.1)
     }
   }
 }
---
+++ src/node/components/draw/draw.ts
@@ -0,0 +1,31 @@
+import { IComponent } from '@/utils'
+import { Node } from '@/node'
+import { CanvasLayer } from '@/canvas-layer'
+import { Settings } from '@/settings'
+
+export class NodeDrawComponent implements IComponent {
+  public Entity: Node
+
+  public Awake(): void {
+    this.Clear()
+  }
+
+  public Update(deltaTime: number): void {
+    this.Clear()
+    this.Draw()
+  }
+
+  private Clear(): void {
+    CanvasLayer.Foreground.ClearRect(this.Entity.Start, this.Entity.Size)
+  }
+
+  private Draw(): void {
+    if (this.Entity.IsActive) {
+      CanvasLayer.Foreground.FillRect(
+        this.Entity.Start,
+        this.Entity.Size,
+        Settings.grid.node.color
+      )
+    }
+  }
+}
